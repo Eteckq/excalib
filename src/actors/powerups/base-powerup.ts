f86dcf2f85ad701900137cf1deaac74a1fa2b4f5
= <=>
2024-04-02 15:26:59 +0200

    destroy powerup

--- src/actors/powerups/base-powerup.ts
+++ src/actors/powerups/base-powerup.ts
@@ -1,8 +1,10 @@
-import { Actor, CollisionType, vec } from "excalibur";
+import { Actor, CollisionType, Engine, Vector, vec } from "excalibur";
 import { Resources } from "../../resources";
 import { PlayerCollisionMask, PowerUpCanCollideWith } from "../../colliders";
+import { HEIGHT, WIDTH } from "../../constants";
 
 export abstract class BasePowerup extends Actor {
+  private speed = 100
   constructor(x: number, y: number) {
     super({
       x,
@@ -12,6 +14,25 @@ export abstract class BasePowerup extends Actor {
       collisionType: CollisionType.Passive,
       collisionGroup: PowerUpCanCollideWith,
     });
-    this.vel = vec(0, 100);
+    this.vel = new Vector(this.speed, this.speed);
+
+  }
+
+  onInitialize(engine: Engine<any>): void {
+    engine.clock.schedule(()=> {
+      this.actions.blink(100, 100, 10).die()
+    }, 6000)
+  }
+
+  onPreUpdate(engine: Engine<any>, delta: number): void {
+    if (this.pos.x < 0) {
+      this.vel.x = this.speed;
+    } else if (this.pos.x > WIDTH) {
+      this.vel.x = -this.speed;
+    } else if (this.pos.y > HEIGHT) {
+      this.vel.y = -this.speed;
+    } else if (this.pos.y < 0) {
+      this.vel.y = this.speed;
+    }
   }
 }
